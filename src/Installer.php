648e634d657a8ae2e9fdabb7281b564eebcb44bb
Hajime MATSUMOTO <m@hajime.work>
2019-12-06 07:19:59 +0900

    udpate

--- src/Installer.php
+++ src/Installer.php
@@ -69,8 +69,8 @@ class Installer
                 return;
             }
             $contents = file_get_contents($filePath);
-            $contents = str_replace('__Vendor__', self::$vendor), $contents);
-            $contents = str_replace('__Package__', self::$project), $contents);
+            $contents = str_replace('__Vendor__', self::$vendor, $contents);
+            $contents = str_replace('__Package__', self::$project, $contents);
             $contents = str_replace('__year__', date('Y'), $contents);
             $contents = str_replace('__name__', self::$name, $contents);
             file_put_contents($filePath, $contents);
